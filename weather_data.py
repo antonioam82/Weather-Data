d52a3dc8179256153856265b200424e25ad7282a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-05-01 16:35:20 +0200

    Update weather_data.py

--- weather_data.py
+++ weather_data.py
@@ -5,6 +5,13 @@ from geopy.geocoders import Nominatim
 from colorama import Fore, init, Style
 import argparse
 
+init()
+
+now = datetime.now()
+day = now.day
+month = now.month
+year = now.year
+
 def main():
     parser = argparse.ArgumentParser(prog="WEATHER DATA 0.0",conflict_handler='resolve',
                                      description="Get wether metrics in command line")
@@ -12,11 +19,21 @@ def main():
     parser.add_argument('-ptn','--point',help="Coords")
     parser.add_argument('-dt','--data',choices=['tavg','tmin','tmax','prcp','snow','wdir','wspd','wpgt','pres','tsun'],help="Wheather metrics")
     parser.add_argument('-per','--periodicity',choices=['Daily','Monthly'],help="Periodicity for time series")
-    parser.add_argument('-st','--start',help="Start date for time series")
-    parser.add_argument('-e','--end',help="End date for time series")
+    parser.add_argument('-st','--start',type=check_dateformat,help="Start date for time series")
+    parser.add_argument('-e','--end',default='{}/{}/{}'.format(year,month,day),type=check_dateformat,help="End date for time series")
     parser.add_argument('-plt','--plot',action='store_true',help="Show graph")
 
     args = parser.parse_args()
+    print(args.end)
+
+def check_dateformat(val):
+    try:
+        print(val)
+        date = datetime.strptime(val, '%Y/%m/%d')
+        return date
+    except Exception as e:
+        raise argparse.ArgumentTypeError(f"BAD DATE FORMAT: {str(e)}")
+    
 
 
 if __name__=='__main__':
