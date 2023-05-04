0e668c29443b1d49729aa9209385cc0d238e5938
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-05-04 16:57:19 +0200

    Update weather_data.py

--- weather_data.py
+++ weather_data.py
@@ -16,8 +16,8 @@ def main():
     parser = argparse.ArgumentParser(prog="WEATHER DATA 0.0",conflict_handler='resolve',
                                      description="Get wether metrics in command line")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-reg','--region',help="Interest region")
-    group.add_argument('-ptn','--point',help="Coords")
+    group.add_argument('-reg','--region',type=set_reg_point,help="Interest region")
+    group.add_argument('-ptn','--point',type=set_point,help="Coords")
     parser.add_argument('-dt','--data',choices=['tavg','tmin','tmax','prcp','snow','wdir','wspd','wpgt','pres','tsun'],help="Wheather metrics")
     parser.add_argument('-per','--periodicity',choices=['Daily','Monthly'],help="Periodicity for time series")
     parser.add_argument('-st','--start',type=check_dateformat,help="Start date for time series")
@@ -28,13 +28,13 @@ def main():
     args = parser.parse_args()
 
     if not args.region and not args.point:
-        parser.error(Fore.RED+Style.BRIGHT+"region or point is required."+Fore.RESET+Style.RESET_ALL) 
+        parser.error(Fore.RED+Style.BRIGHT+"region or point is required."+Fore.RESET+Style.RESET_ALL)
     
     if args.end > args.start:
         parser.error(Fore.RED+Style.BRIGHT+"start date must be smaller than end date."+Fore.RESET+Style.RESET_ALL) 
     
-    print(args.start)
-    print(args.end)
+    #print(args.start)
+    #print(args.end)
 
 def check_dateformat(val):
     try:
@@ -42,8 +42,26 @@ def check_dateformat(val):
         return date
     except Exception as e:
         raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+f"BAD DATE FORMAT: {str(e)}"+Fore.RESET+Style.RESET_ALL)
-    
 
+def set_reg_point(val):
+    try:
+        loc = Nominatim(user_agent="GetLoc")
+        getLoc = loc.geocode(val)
+        points = [getLoc.latitude,getLoc.longitude]
+        print(points)
+        return points
+    except Exception as e:
+        raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)
+
+def set_point(val):
+    try:
+        vals = val.split("-")
+        points = [vals[0],vals[1]]
+        print(points)
+        return points
+    except Exception as e:
+        raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)
+        
 
 if __name__=='__main__':
     main()
